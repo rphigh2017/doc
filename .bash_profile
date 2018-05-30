97e540cd2c34dc5b0daac877ef22d0747c9e0ce6
rphigh2017 <10490539@qq.com>
2018-05-30 14:39:46 +0800

    update

---
+++ .bash_profile
@@ -0,0 +1,8 @@
+export LS_OPTIONS='--color=auto'
+export CLICOLOR='Yes'
+export LSCOLORS='exfxcxdxbxegedabagacad'
+
+export PS1='\u @ \w $ '
+
+alias vim=mvim
+alias ll='ls -l'
--- auto_update.sh
+++ auto_update.sh
@@ -6,7 +6,8 @@ cp ~/.vim/doc/* ./vimdoc
 cp ~/.vim/template/* ./vimtemplate
 cp ~/.vim/dictionary/* ./vimdictionary
 cp ~/.gvimrc ${root_path}/.gvimrc
+cp ~/.bash_profile ${root_path}/.bash_profile
 
-git add ${root_path}/* ${root_path}/.gvimrc
+git add ${root_path}/* ${root_path}/.gvimrc ${root_path}/.bash_profile
 git commit -m "update"
 git push -u origin master
